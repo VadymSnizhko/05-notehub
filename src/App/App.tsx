f35fdeea30c8ac4dff36514186a61b141f47dc03
VadymSnizhko <snizhko@gmail.com>
2025-06-05 23:51:42 +0300

    Base project for home work 05 React

---
+++ src/App/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import css from './App.module.css'
+
+function App() {
+
+  return (
+<div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>
+  )
+}
+
+export default App
