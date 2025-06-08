b39cc9bc0f86f7a1420d1ba08a9e67a812df460a
VadymSnizhko <snizhko@gmail.com>
2025-06-08 23:40:38 +0300

    Fix structure project for home work 05 React

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 import './index.css'
 import App from './App/App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<QueryClientProvider client={queryClient}>
+  <App />
+</QueryClientProvider>,
 )
--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,5 +1,14 @@
-export function fetchNotes(){
-    
+import axios from 'axios';
+import {type Note, type CreateNoteDto} from '../types/note'
+
+const BASE_URL = 'https://notehub-public.goit.study/api/notes';
+
+export function fetchNotes = async(search = ''): Promise<Note[]> => {
+    const res = await axios.get(BASE_URL, {
+    params: { search },
+    headers: { Authorization: `Bearer ${import.meta.env.TOKEN_NOTEHUB}` },
+  });
+    return res.data;
 }
 
 export function createNote(){
