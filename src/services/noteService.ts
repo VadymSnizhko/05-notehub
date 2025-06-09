072fff21b35174ed099656318f8e6496ac403374
vadim <snizhko@gmail.com>
2025-06-10 01:31:07 +0300

    next step v2.0

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,21 +1,17 @@
-import axios from 'axios';
-import { useMutation } from '@tanstack/react-query';
-import {type Note} from '../types/note'
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { type Note } from "../types/note";
 
-const BASE_URL = 'https://notehub-public.goit.study/api/notes';
+const BASE_URL = "https://notehub-public.goit.study/api/notes";
 
-export const fetchNotes = async(search: string):Promise<Note[]> => {
-    const res = await axios.get(BASE_URL, {
+export const fetchNotes = async (): Promise<Note[]> => {
+  const res = await axios.get(BASE_URL, {
     params: { search },
     headers: { Authorization: `Bearer ${import.meta.env.TOKEN_NOTEHUB}` },
   });
-    return res.data;
-}
+  return res.data;
+};
 
-export function createNote(){
+export function createNote() {}
 
-}
-
-export function deleteNote(){
-    
-}
+export function deleteNote() {}
