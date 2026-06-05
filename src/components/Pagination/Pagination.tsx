fdacc424b4d41a5b12a4803a446413a755aa2a33
Vadym Snizhko <48770880+VadymSnizhko@users.noreply.github.com>
2026-06-05 22:44:32 +0300

    I hope last Update Pagination.tsx

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,9 +1,20 @@
-import ReactPaginate from 'react-paginate';
+import type { ComponentType } from "react";
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+
+type ModuleWithDefault<T> = {
+  default: T;
+};
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<
+    ComponentType<ReactPaginateProps>
+  >
+).default;
 
 interface PaginationProps {
   totalPages: number;
   currentPage: number;
-  //onPageChange: (selectedItem: { selected: number }) => void;
   onPageChange: (page: number) => void;
 }
 
@@ -17,9 +28,9 @@ function Pagination({
       pageCount={totalPages}
       pageRangeDisplayed={3}
       marginPagesDisplayed={1}
-      //onPageChange={onPageChange}
-      onPageChange={(selectedItem) => onPageChange(selectedItem.selected + 1)
-}
+      onPageChange={(selectedItem) =>
+        onPageChange(selectedItem.selected + 1)
+      }
       forcePage={currentPage - 1}
       previousLabel="<"
       nextLabel=">"
